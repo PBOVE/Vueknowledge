ebaa7cd8f50c50788301f6e6b20bae991ad39057
站望看 <771757562@qq.com>
2019-12-08 23:22:35 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+/**
+ *
+ *  @author ZTiger
+ *
+ */
+
+
+import Vue from 'vue'
+import vueRouter from 'vue-router'
+
+/**
+ *  引入 view UI
+ */
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+
+
+
+
+
+
+
+
+
+
+Vue.use(ViewUI);
+Vue.use(vueRouter);
+
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer:{
+        proxy:{
+            '/api':{
+                target:"http://119.29.218.18:9090/",
+                changeOrigin: true,
+            }
+        }
+    }
+}
