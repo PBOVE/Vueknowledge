317b5e67a5ff1d3d2f0ec186a2c42b52bce9faea
pbove <771757562@qq.com>
2019-12-09 00:44:46 +0800

    updata config

--- src/router/index.js
+++ src/router/index.js
@@ -14,16 +14,38 @@ import vueRouter from 'vue-router'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 
+/**
+ * 引入 vuex
+ */
+import Vuex from 'vuex'
 
+/**
+ *  引入 axios
+ */
+import axios from 'axios'
 
+/**
+ * 引入路由组件
+ */
+import helloWorld from '../components/HelloWorld.vue'
 
-
-
-
-
-
-
-
+Vue.use(Vuex)
 Vue.use(ViewUI);
 Vue.use(vueRouter);
+Vue.prototype.axios = axios;
 
+
+/**
+ * 路由 跳转
+ */
+let router = new vueRouter({
+    routes:[
+        {
+            path:'/',
+            name:'helloWorld',
+            component:helloWorld
+        }
+    ]
+})
+
+export default  router
