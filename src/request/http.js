785a62bc7f13e3d5d84e897df50493f3d1498631
站望看 <771757562@qq.com>
2019-12-11 12:32:28 +0800

    测试

--- src/request/http.js
+++ src/request/http.js
@@ -6,7 +6,8 @@
 // window.console.log(process.env.VUE_APP_MODE);
 //axios 上传地址
 let baseUrl = process.env.VUE_APP_API_URL;
-
+window.console.log(process.env.VUE_APP_API_URL);
+window.console.log(process.env.VUE_APP_MODE);
 export {
     baseUrl
 }
