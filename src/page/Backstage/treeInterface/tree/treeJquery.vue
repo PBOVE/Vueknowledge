1701a3d371744debabf164a38191196ca980877f
pbove <771757562@qq.com>
2020-02-04 02:10:13 +0800

    add tree drop

--- src/page/Backstage/treeInterface/tree/treeJquery.vue
+++ src/page/Backstage/treeInterface/tree/treeJquery.vue
@@ -12,11 +12,11 @@
 </template>
 
 <script>
-import $ from "../../../../assets/jquery-vendor";
-import "ztree";
-import "ztree/css/metroStyle/metroStyle.css";
+import $ from '../../../../assets/jquery-vendor';
+import 'ztree';
+import 'ztree/css/metroStyle/metroStyle.css';
 export default {
-  props: ["treelistVal", "itemId"],
+  props: ['treelistVal', 'itemId'],
   data() {
     return {
       //设置 ztree 树
@@ -26,35 +26,45 @@ export default {
           showIcon: false,
           selectedMulti: true,
           dblClickExpand: true,
-          addDiyDom: this.addDiyDom
+          addDiyDom: this.addDiyDom,
         },
         data: {
           simpleData: {
-            enable: true
-          }
+            enable: true,
+          },
+        },
+        edit: {
+          drag: {
+            isMove: true,
+            inner: true,
+          },
+          enable: true,
+          showRemoveBtn: false,
+          showRenameBtn: false,
         },
         callback: {
           beforeDblClick: this.showChildClik,
           beforeExpand: this.showChildClik,
-          onClick: this.zTreeOnClick
-        }
+          onClick: this.zTreeOnClick,
+          beforeDrop: this.zTreeBeforeDrop,
+          beforeDragOpen: this.zTreeBeforeDragOpen,
+        },
       },
       //点击后ztree节点 id
-      StreeId: "",
+      StreeId: '',
       // 点击后ztree 节点
-      StreeNode: "",
+      StreeNode: '',
       // 树
-      zTree: ""
+      zTree: '',
     };
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     //获取服务器数据
     getTreeData() {
-      const url = "node";
+      const url = 'node';
       const obj = {
-        itemId: this.itemId
+        itemId: this.itemId,
       };
       this.get(url, obj)
         .then(res => {
@@ -65,12 +75,12 @@ export default {
     // 请求数据处理
     HandleData(data) {
       let Arr = [];
-      this.$emit("selectNode", 9, data.length);
+      this.$emit('selectNode', 9, data.length);
       data.forEach(item => {
         let obj = {
           id: item.id,
           name: item.name,
-          isParent: item.child
+          isParent: item.child,
         };
         Arr.push(obj);
       });
@@ -78,47 +88,36 @@ export default {
     },
     //添加节点 修改样式
     addDiyDom(treeId, treeNode) {
-      let spaceWidth = 1.5;
-      let switchObj = $("#" + treeNode.tId + "_switch");
-      let icoObj = $("#" + treeNode.tId + "_ico");
+      let spaceWidth = 15;
+      let switchObj = $('#' + treeNode.tId + '_switch');
+      let icoObj = $('#' + treeNode.tId + '_ico');
       switchObj.remove();
       icoObj.before(switchObj);
       if (treeNode.level > 0) {
-        let spaceStr =
-          "<span style='display: inline-block;width:" +
-          spaceWidth * treeNode.level +
-          "em'></span>";
+        let spaceStr = `<span  id='${treeNode.tId}_space' style='display: inline-block;width:${spaceWidth * treeNode.level}px'></span>`;
         switchObj.before(spaceStr);
       }
     },
-    // sord 排序
-    sortId(ObjA, ObjB) {
-      var valA = ObjA.sortId;
-      var valB = ObjB.sortId;
-      if (valA < valB) return -1;
-      else if (valA > valB) return 1;
-      else return 0;
-    },
     //创建 树图 修改样式
     createTree(nodes) {
-      $.fn.zTree.init($("#ztreeDemo"), this.selectTreeSetting, nodes);
-      this.zTree = $.fn.zTree.getZTreeObj("ztreeDemo");
+      $.fn.zTree.init($('#ztreeDemo'), this.selectTreeSetting, nodes);
+      this.zTree = $.fn.zTree.getZTreeObj('ztreeDemo');
     },
     //点击 节点 前 响应函数
     beforeMouseUp(treeId, treeNode) {
       if (!treeNode) return;
       this.StreeId = treeNode.tId;
       this.StreeNode = treeNode;
-      this.$emit("selectNode", 1, true);
-      this.$emit("selectNode", 2, treeNode);
+      this.$emit('selectNode', 1, true);
+      this.$emit('selectNode', 2, treeNode);
     },
     // 双击 节点 展开 节点 函数
-    showChildClik(treeId, treeNode) {
+    showChildClik(treeId, treeNode, callback) {
       if (!treeNode) return;
       if (!treeNode.isParent || treeNode.asyncParent) return 1;
       //异步加载 防止重复加载
       treeNode.asyncParent = true;
-      let url = "node/" + treeNode.id + "/child";
+      let url = 'node/' + treeNode.id + '/child';
       this.get(url)
         .then(res => {
           let data = res.data;
@@ -127,10 +126,13 @@ export default {
             Arr.push({
               id: item.id,
               name: item.name,
-              isParent: item.child
+              isParent: item.child,
             });
           });
           this.zTree.addNodes(treeNode, Arr, false);
+          if (callback) {
+            callback();
+          }
         })
         .catch(() => {});
     },
@@ -138,72 +140,114 @@ export default {
     zTreeOnClick(event, treeId, treeNode) {
       if (!treeNode) return;
       else if (event.ctrlKey) {
-        this.$emit("selectNode", 10, this.zTree.getSelectedNodes());
+        this.$emit('selectNode', 10, this.zTree.getSelectedNodes());
       } else {
         this.StreeId = treeNode.tId;
         this.StreeNode = treeNode;
-        this.$emit("selectNode", 1, true);
-        this.$emit("selectNode", 2, treeNode);
-        this.$emit("selectNode", 11, []);
-      }
-    }
-  },
-  watch: {
-    "treelistVal.ExitName": {
-      handler: function(val) {
-        $("#" + this.StreeId + "_a").attr("title", val);
-        $("#" + this.StreeId + "_span").text(val);
-        this.StreeNode["name"] = val;
-        this.$emit("selectNode", 2, this.StreeNode);
+        this.$emit('selectNode', 1, true);
+        this.$emit('selectNode', 2, treeNode);
+        this.$emit('selectNode', 11, []);
       }
     },
-    "treelistVal.addName": {
+    // 用于捕获拖拽节点移动到折叠状态的父节点后
+    zTreeBeforeDragOpen() {
+      return false;
+    },
+    // 用于捕获节点拖拽操作结束之前的事件回调函数，并且根据返回值确定是否允许此拖拽操作
+    zTreeBeforeDrop(treeId, treeNodes, targetNode, moveType) {
+      if (!(targetNode === null || moveType !== 'inner')) {
+        let treeNode = treeNodes[0];
+        if (this.showChildClik('', targetNode, this.pushServeDrop(treeNode,targetNode)) === 1) {
+          // 加载过数据
+          this.pushServeDrop(treeNode,targetNode);
+        }
+      }
+      return false;
+    },
+    // 拖拽结束,并向服务器发送请求成功
+    DropSuccess(treeNodes) {
+      let spaceWidth = 15;
+      let treeNodeBFS = Node => {
+        let spaceNum = parseInt($(`#${Node.parentTId}_space`).css('width') || 0) / spaceWidth + 1;
+        $(`#${Node.tId}_space`).css('width', spaceWidth * spaceNum + 'px');
+        if (Node.isParent && Node.asyncParent) {
+          let childNode = Node.children;
+          childNode.forEach(item => {
+            treeNodeBFS(item);
+          });
+        }
+      };
+      treeNodes.forEach(item => {
+        treeNodeBFS(item);
+      });
+    },
+    // 向服务器发送拖拽的
+    pushServeDrop(treeNode, targetNode) {
+      const URL = `node/${treeNode.id}/movement?target=${targetNode.id}`;
+      this.put_json(URL)
+        .then(() => {
+          this.zTree.moveNode(targetNode, treeNode, true);
+          this.DropSuccess([treeNode]);
+        })
+        .catch(() => {});
+    },
+  },
+  watch: {
+    'treelistVal.ExitName': {
+      handler: function(val) {
+        $('#' + this.StreeId + '_a').attr('title', val);
+        $('#' + this.StreeId + '_span').text(val);
+        this.StreeNode['name'] = val;
+        this.$emit('selectNode', 2, this.StreeNode);
+      },
+    },
+    'treelistVal.addName': {
       handler: function(val) {
         if (this.StreeNode) {
           //判断 是否双击 点击过
-          if (this.showChildClik("", this.StreeNode) === 1) {
+          if (this.showChildClik('', this.StreeNode) === 1) {
             this.StreeNode.asyncParent = true;
             this.zTree.addNodes(this.StreeNode, {
               id: val.id,
               name: val.name,
-              isParent: false
+              isParent: false,
             });
           }
         } else {
           this.zTree.addNodes(null, {
             id: val.id,
             name: val.name,
-            isParent: false
+            isParent: false,
           });
         }
-      }
+      },
     },
-    "treelistVal.delName": {
+    'treelistVal.delName': {
       handler: function() {
         this.zTree.removeNode(this.StreeNode, false);
-        this.StreeNode = "";
-        this.$emit("selectNode", 1, false);
-      }
+        this.StreeNode = '';
+        this.$emit('selectNode', 1, false);
+      },
     },
-    "treelistVal.delNodes": {
+    'treelistVal.delNodes': {
       handler: function() {
         let Nodes = this.zTree.getSelectedNodes();
         Nodes.forEach(item => {
           if (this.StreeNode.id === item.id) {
-            this.StreeNode = "";
-            this.$emit("selectNode", 1, false);
-            this.$emit("selectNode", 13);
+            this.StreeNode = '';
+            this.$emit('selectNode', 1, false);
+            this.$emit('selectNode', 13);
           }
           this.zTree.removeNode(item, false);
         });
         if (this.StreeNode) {
           this.zTree.selectNode(this.StreeNode);
-          this.$emit("selectNode", 1, true);
+          this.$emit('selectNode', 1, true);
         }
-        this.$emit("selectNode", 11, []);
-      }
-    }
-  }
+        this.$emit('selectNode', 11, []);
+      },
+    },
+  },
 };
 </script>
 
@@ -283,14 +327,29 @@ export default {
 .ztree li span.button.roots_close::before,
 .ztree li span.button.bottom_close::before,
 .ztree li span.button.center_close::before {
-  content: "\f11f";
+  content: '\f11f';
   font-size: 15px;
 }
 .ztree li span.button.root_open::before,
 .ztree li span.button.roots_open::before,
 .ztree li span.button.bottom_open::before,
 .ztree li span.button.center_open::before {
-  content: "\F116";
+  content: '\F116';
   font-size: 15px;
+}
+.ztree li a.tmpTargetNode_inner {
+  height: 30px;
+  background: #2d8cf0;
+  border: none;
+}
+ul.ztree.zTreeDragUL {
+  z-index: 12;
+  border: none;
+  padding: 0 20px 0 0;
+  background: #f8f8f9;
+  border-radius: 8px;
+}
+span.tmpzTreeMove_arrow {
+  background-image: none;
 }
 </style>
